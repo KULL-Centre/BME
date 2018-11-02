301665da8a4755767987be671ce39fe412f429ba
sbottaro <sbottaro@sissa.it>
2018-11-02 09:58:18 +0100

    fix example1

--- examples/example1.py
+++ examples/example1.py
@@ -21,10 +21,10 @@ rew = bme.Reweight()
 rew.load(exp_couplings,calc_couplings)
 
 # do optimization using theta=2
-chib,chia, srel = rew.optimize(theta=0.007)
+chib,chia, srel = rew.optimize(theta=2)
 
-# compare NOE, uNOE and J Couplings before and after optimization .
-# output is written to files with prefix 'example1'
+# compare NOE  before and after optimization .
+# output is written to files with prefix 'example1_noe'
 chi2_b,chi2_a = rew.weight_exp(exp_noe,calc_noe, 'example1_noe')
 
 # print chisquared
